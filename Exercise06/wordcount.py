3f5d82b0580a22fa9610b11bec1a0efc7c849851
Marissa <yaridog@gmail.com>
2013-03-11 22:47:55 -0700

    solution sorting by frequency and alphabetical order

--- Exercise06/wordcount.py
+++ Exercise06/wordcount.py
@@ -1,77 +1,49 @@
-#open file and read it
+# open file and read it
 from sys import argv
-
 script, fileinput = argv
 
-opened = open(fileinput)
-
-read_txt = opened.read()
-
+f = open(fileinput)
+read_txt = f.read()
 txt = read_txt.lower()
-
+# split the text to create a list of words
 split_txt = txt.split()
 
+# create empty dict
 word_dict = {}
-# split the text 
 
-for i, v in enumerate(split_txt):
-	stripped_word = v.rstrip('.,:-;!?')
-	stripped_word = stripped_word.lstrip('.,:-')
+# loop through each word in the split_txt list, store the word and count in a dict
+for v in split_txt:
+	# for each word, remove punctuation from beginning and end of words
+	stripped_word = v.rstrip('.,:-_;!?"()\'')
+	stripped_word = stripped_word.lstrip('.,:_-"()\'')
+	# test if word is already in the dict, increase count if found
 	if stripped_word in word_dict:
 		word_dict[stripped_word] = word_dict[stripped_word] + 1
 	else:
+		# otherwise add the word and word count to dict
 		word_dict.setdefault(stripped_word, 1)
-	# print stripped_word, word_dict[stripped_word]
 
-
-
+# word_dict now contains the word and frequency of that word as a key=>value pair
+# use .items() method to create a list of tuples containing the word and count
+# this results in a list of tuples (ie [('word1, 3','word2', 1)])
 tupled_list = word_dict.items()
+
+# need to reassign the values in the tuple so it's (count,value) because it's storted by the first value
+# create an empty list to store the switched values of the tuples
 new_list = []
-print new_list
 
-
-# #print sorted(tupled_list)
-# sorted_word_list = {}
 for word, count in tupled_list:
- 	print word, count
+	# reassign the contents of the tuple so the count is the first item and word as the second
+ 	new_tuple = (count, word)
+ 	# add the reorganized tuple to the new list
+ 	new_list.append(new_tuple)
 
+# in order to sort by 2 values, need to use lambda    
+# sort by secondary key (the word)
+new_list.sort(key=lambda x:x[1])
+# then sort by the first key (the count) and reverse
+new_list.sort(key=lambda x:x[0], reverse=True)
 
-
- 	
-# 	#print i, v
-# 	for index, tupleval in enumerate(v):
-# 		count = v[1]
-# 		word = v[0]
-# 		#tuple2 = word
-# 		sorted_word_list[count] = word
-
-# new_sorted_list = sorted_word_list.items()
-# #print new_sorted_list
-# print new_sorted_list[::-1]
-# final_sort = sorted(new_sorted_list)
-# final_sort = new_sorted_list.sort()
-
-	#print tuple1,tuple2
-#words as keys
-#values as count		
-
-
-
-# iterate over list of words
-
-# strip the text to remove punctuation
-
-
-# test if key(word) exists
-
-# if it doesn't exists create set default
-
-# if it does, increase the value by one
-
-
-
-# string.split()
-# string.strip()
-# dict.setdefault()
-# dict.iteritems()
+for count, word in new_list:
+	print count,word
 
